014f133c270989e6f7d4f005fe166b667d706c97
agarciaprogrammer <AgustinNicolas.Garcia@alumnos.uai.edu.ar>
2025-07-13 00:44:49 -0300

    Remove data, uploads, node_modules, dist

--- backend/dist/routes/biografiaRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// /routes/biografiaRoutes.ts
-const express_1 = require("express");
-const biografiaController_1 = require("../controllers/biografiaController");
-const router = (0, express_1.Router)();
-router.get('/', biografiaController_1.getBiografia);
-router.put('/', biografiaController_1.updateBiografia);
-exports.default = router;
