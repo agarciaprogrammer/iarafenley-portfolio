65cbb05c95dfa9c67d5e2d9b4aa8575c7cc96906
agarciaprogrammer <AgustinNicolas.Garcia@alumnos.uai.edu.ar>
2025-09-19 15:29:47 -0300

    expoService modified

--- frontend/src/services/expoServices.ts
+++ frontend/src/services/expoServices.ts
@@ -2,18 +2,18 @@ import { apiFetch } from "./api";
 import type { Exposicion } from '../types'
 
 export const getExposiciones = async (): Promise<Exposicion[]> => {
-    return await apiFetch('/api/expo')
+    return await apiFetch('/api/exposiciones')
 }
 
 export const updateExposicion = async ({ id, text }: Exposicion): Promise<Exposicion> => {
-    return await apiFetch(`/api/expo/${id}`, {
+    return await apiFetch(`/api/exposiciones/${id}`, {
         method: 'PUT',
         body: JSON.stringify({ text }),
     })    
 }
 
 export const crearExpo = async (text: string): Promise<Exposicion> => {
-  return await apiFetch('/api/expo', {
+  return await apiFetch('/api/exposiciones', {
     method: 'POST',
     body: JSON.stringify({ text }),
   });
