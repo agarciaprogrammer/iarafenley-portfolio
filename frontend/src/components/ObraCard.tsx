8ff93b132ecdd9ff881a387a4da0acc5e31bff46
agarciaprogrammer <AgustinNicolas.Garcia@alumnos.uai.edu.ar>
2025-09-19 15:23:50 -0300

    obracard mod

--- frontend/src/components/ObraCard.tsx
+++ frontend/src/components/ObraCard.tsx
@@ -2,11 +2,9 @@ import type { Obra } from '../types'
 import '../styles/Obras.css'
 
 const ObraCard = ({ src, titulo, tecnica, anio }: Obra) => {
-  const fullSrc = `${import.meta.env.VITE_BACKEND_URL}${src}`
-
   return (
     <div className="obra-card">
-      <img src={fullSrc} alt={titulo || 'Obra de arte'} className="obra-img" />
+      <img src={src} alt={titulo || 'Obra de arte'} className="obra-img" />
       {(titulo || tecnica || anio) && (
         <div className="obra-info">
           {titulo && <p><strong>{titulo}</strong></p>}
@@ -17,5 +15,6 @@ const ObraCard = ({ src, titulo, tecnica, anio }: Obra) => {
     </div>
   )
 }
+
 
 export default ObraCard
